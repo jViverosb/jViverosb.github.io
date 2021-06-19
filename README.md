a738d6889aca8811e42a67e41617b1bdf1368c67
jViverosb <86033726+jViverosb@users.noreply.github.com>
2021-06-18 22:21:18 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# jviverosb.github.io
+Proyecto web
+https://jviverosb.github.io/
---
+++ js/script.js
@@ -0,0 +1,70 @@
+
+const inputBox = document.querySelector(".inputField input");
+const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+const deleteAllBtn = document.querySelector(".footer button");
+
+inputBox.onkeyup = ()=>{
+  let userEnteredValue = inputBox.value; 
+  if(userEnteredValue.trim() != 0){ 
+    addBtn.classList.add("active"); 
+  }else{
+    addBtn.classList.remove("active"); 
+  }
+}
+
+showTasks(); 
+
+addBtn.onclick = ()=>{ 
+  let userEnteredValue = inputBox.value; 
+  let getLocalStorageData = localStorage.getItem("Nueva tarea"); 
+  if(getLocalStorageData == null){ 
+    listArray = []; 
+  }else{
+    listArray = JSON.parse(getLocalStorageData);  
+  }
+  listArray.push(userEnteredValue); 
+  localStorage.setItem("Nueva tarea", JSON.stringify(listArray)); 
+  showTasks(); 
+  addBtn.classList.remove("active"); 
+}
+
+function showTasks(){
+  let getLocalStorageData = localStorage.getItem("Nueva tarea");
+  if(getLocalStorageData == null){
+    listArray = [];
+  }else{
+    listArray = JSON.parse(getLocalStorageData); 
+  }
+  const pendingTasksNumb = document.querySelector(".pendingTasks");
+  pendingTasksNumb.textContent = listArray.length; 
+  if(listArray.length > 0){ 
+    deleteAllBtn.classList.add("active"); 
+  }else{
+    deleteAllBtn.classList.remove("active"); 
+  }
+  let newLiTag = "";
+  listArray.forEach((element, index) => {
+    newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
+  });
+  todoList.innerHTML = newLiTag; 
+  inputBox.value = ""; 
+}
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+deleteAllBtn.onclick = ()=>{
+  let getLocalStorageData = localStorage.getItem("Nueva tarea"); 
+  if(getLocalStorageData == null){ 
+    listArray = []; 
+  }else{
+    listArray = JSON.parse(getLocalStorageData);  
+    listArray = []; 
+  }
+  localStorage.setItem("Nueva tarea", JSON.stringify(listArray)); 
+  showTasks(); 
+}
